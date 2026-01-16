d2ba46265c4518484368509aa3f4c300ceea9514
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-01-16 19:12:37 +0100

    refactor(pages/map, features/map/components/map-view): extract Leaflet map logic and template from map page into feature child component

---
+++ src/app/features/map/components/map-view/map-view.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map-view',
+  imports: [],
+  templateUrl: './map-view.html',
+  styleUrl: './map-view.css',
+})
+
+export class MapViewComponent implements OnInit {
+  
+  private map: any;
+  private userMarker: L.Marker<any> | undefined;
+
+  ngOnInit(): void {
+      this.initMap();
+  }
+
+  private initMap() {
+    this.map = L.map('map').setView([41.478, 2.310], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map);
+  }
+
+  getlocation() {
+    if (navigator.geolocation) {
+
+      // const myIcon = L.icon({
+      //   iconUrl:'aqui va la url de la ruta del icono porque este no me funciona',
+      //   iconSize:[ 25,40 ]
+      // })
+
+      navigator.geolocation.getCurrentPosition((position) => {
+        const coords : [ number, number ] = [ position.coords.latitude, position.coords.longitude ];
+
+        if (this.userMarker) {
+          this.userMarker = L.marker(coords);
+        } else {
+          this.userMarker = L.marker(coords, {
+            draggable:true,
+            /* icon:myIcon, */
+          }).addTo(this.map).bindPopup('You vehicle are here').openPopup();
+
+          this.userMarker.on('dragend', (event) => {
+            const marker = event.target;
+            const position = marker.getLatLng();
+            marker.setLatLng(position).openPopup();
+            this.map.setView(position, 19);
+            console.log(`Marcador : ${position.lat}, ${position.lng}`)
+          })
+        }
+
+        this.map.setView(coords, 19);
+
+      }, () => {
+        alert('No se pudo obtener la geolocalización')
+      });
+
+    } else {
+      alert("Geolocalización no soportada por el navegador");
+    }
+  }
+  
+}
--- src/app/pages/map/map.ts
+++ src/app/pages/map/map.ts
@@ -1,65 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import * as L from 'leaflet';
+import { Component } from '@angular/core';
+import { MapViewComponent } from "../../features/map/components/map-view/map-view";
 @Component({
   selector: 'app-map',
   standalone: true,
-  imports: [],
+  imports: [ MapViewComponent ],
   templateUrl: './map.html',
   styleUrls: ['./map.css'],
 })
 
-export class MapComponent implements OnInit {
-
-  private map: any;
-  private userMarker: L.Marker<any> | undefined;
-
-  ngOnInit(): void {
-      this.initMap();
-  }
-
-  private initMap() {
-    this.map = L.map('map').setView([41.478, 2.310], 13);
-
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map);
-  }
-
-  getlocation() {
-    if (navigator.geolocation) {
-
-      // const myIcon = L.icon({
-      //   iconUrl:'aqui va la url de la ruta del icono porque este no me funciona',
-      //   iconSize:[ 25,40 ]
-      // })
-
-      navigator.geolocation.getCurrentPosition((position) => {
-        const coords : [ number, number ] = [ position.coords.latitude, position.coords.longitude ];
-
-        if (this.userMarker) {
-          this.userMarker = L.marker(coords);
-        } else {
-          this.userMarker = L.marker(coords, {
-            draggable:true,
-            /* icon:myIcon, */
-          }).addTo(this.map).bindPopup('You vehicle are here').openPopup();
-
-          this.userMarker.on('dragend', (event) => {
-            const marker = event.target;
-            const position = marker.getLatLng();
-            marker.setLatLng(position).openPopup();
-            this.map.setView(position, 19);
-            console.log(`Marcador : ${position.lat}, ${position.lng}`)
-          })
-        }
-
-        this.map.setView(coords, 19);
-
-      }, () => {
-        alert('No se pudo obtener la geolocalización')
-      });
-
-    } else {
-      alert("Geolocalización no soportada por el navegador");
-    }
-  }
+export class MapComponent {
 
 }
