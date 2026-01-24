fa862f6df131d8f179e3c79d3020efa0d7fa63fc
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-01-24 19:44:19 +0100

    fix(HomeComponent): change order of @if-else to prioritize vehicle list over empty state

--- src/app/shared/components/vehicle-empty-state/vehicle-empty-state.spec.ts
+++ src/app/shared/components/vehicle-empty-state/vehicle-empty-state.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { VehicleEmptyState } from './vehicle-empty-state';
+import { VehicleEmptyStateComponent } from './vehicle-empty-state';
 
-describe('VehicleEmptyState', () => {
-  let component: VehicleEmptyState;
-  let fixture: ComponentFixture<VehicleEmptyState>;
+describe('VehicleEmptyStateComponent', () => {
+  let component: VehicleEmptyStateComponent;
+  let fixture: ComponentFixture<VehicleEmptyStateComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [VehicleEmptyState]
+      imports: [VehicleEmptyStateComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(VehicleEmptyState);
+    fixture = TestBed.createComponent(VehicleEmptyStateComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
