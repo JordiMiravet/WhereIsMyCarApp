f57c3908856b1d20850cd707dc8f522678af6ef5
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-01-26 18:53:42 +0100

    refactor(vehicle-empty-state): relocate VehicleEmptyStateComponent from shared/components to features/vehicle/components to centralize vehicle UI

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -5,7 +5,7 @@ import { VehicleFormModalComponent } from '../../features/vehicle/modals/vehicle
 import { ConfirmModalComponent } from "../../shared/components/modals/confirm-modal/confirm-modal";
 import { CreateButtonComponent } from "../../shared/components/buttons/create-button/create-button";
 import { VehicleService } from '../../features/vehicle/services/vehicle-service/vehicle-service';
-import { VehicleEmptyStateComponent } from "../../shared/components/vehicle-empty-state/vehicle-empty-state";
+import { VehicleEmptyStateComponent } from "../../features/vehicle/components/vehicle-empty-state/vehicle-empty-state";
 import { VehicleModalStateService } from '../../features/vehicle/services/vehicle-modal-state-service/vehicle-modal-state-service';
 import { GeolocationService } from '../../shared/services/geolocation/geolocation-service';
 import { VehicleTableComponent } from "../../features/vehicle/components/vehicle-table/vehicle-table";
--- src/app/pages/map/map.ts
+++ src/app/pages/map/map.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { MapViewComponent } from "../../features/map/components/map-view/map-view";
 import { VehicleService } from '../../features/vehicle/services/vehicle-service/vehicle-service';
-import { VehicleEmptyStateComponent } from "../../shared/components/vehicle-empty-state/vehicle-empty-state";
+import { VehicleEmptyStateComponent } from "../../features/vehicle/components/vehicle-empty-state/vehicle-empty-state";
 import { VehicleModalStateService } from '../../features/vehicle/services/vehicle-modal-state-service/vehicle-modal-state-service';
 import { VehicleFormModalComponent } from "../../features/vehicle/modals/vehicle-form-modal/vehicle-form-modal";
 import { VehicleInterface } from '../../features/vehicle/interfaces/vehicle';
