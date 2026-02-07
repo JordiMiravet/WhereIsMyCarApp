7a674db504b8ec91321a88f0a8ec551d67898fa4
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-02-07 20:29:18 +0100

    feat(calendar): add new calendar options and update styles in CalendarComponent

--- src/app/pages/calendar/calendar.ts
+++ src/app/pages/calendar/calendar.ts
@@ -3,8 +3,10 @@ import { Component, signal, ViewChild, ViewEncapsulation } from '@angular/core';
 
 import { FullCalendarComponent, FullCalendarModule } from '@fullcalendar/angular';
 import { EventInput } from '@fullcalendar/core';
+
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin, { DateClickArg } from '@fullcalendar/interaction';
+import timeGridPlugin from '@fullcalendar/timegrid';
 
 @Component({
   selector: 'app-calendar',
@@ -20,6 +22,8 @@ export class CalendarComponent {
 
   // Esto lo dejo así ahora pero luego tengo que pasarlo a eventos dinamicos 
   calendarEvents: EventInput[] = [
+    { title: 'Peluquero', date: '2026-02-11' },
+    { title: 'Barbero', date: '2026-02-11' },
     { title: 'Cita', date: '2026-02-11' },
     { title: 'Revisión', date: '2026-02-12' },
     { title: 'Mantenimiento', date: '2026-02-11' }
@@ -27,25 +31,41 @@ export class CalendarComponent {
 
   selectedDayEvents = signal<EventInput[]>([]);
   selectedDate = signal<string>('');
+  selectedEvent = signal<string>('');
   
   calendarOptions = {
-    plugins: [ dayGridPlugin, interactionPlugin ],
+    plugins: [ dayGridPlugin, interactionPlugin, timeGridPlugin ],
+
     initialView: 'dayGridMonth',
-    weekends: true,
     headerToolbar: {
-      left: 'prev,next today',
+      left: 'prev,next',
       center: 'title',
-      right: ''
+      right: 'today'
     },
+    showNonCurrentDates: true,
+    fixedWeekCount: false,
+    weekends: true,
+    firstDay: 1,
+    
+    nowIndicator: true,
     selectable: true,
-    dateClick: (arg : DateClickArg) => this.handleDateClick(arg),
-    events: this.calendarEvents,
+    
     height: 'auto',
-    contentHeight: 'auto'
+    contentHeight: 'auto',
+    
+    dateClick: (arg : DateClickArg) => this.handleDateClick(arg),
+    eventDidMount: (info : any ) => { info.el.title = info.event.title},
+    events: this.calendarEvents,
+    dayMaxEvents: 2,
+    moreLinkContent: (arg: any) => { return `+ ${arg.num}`}, /* Esto no me convence como queda, pero por ahora lo dejo así */
+    dayMaxEventRows: false,
+    eventDisplay: 'list-item',
+    editable: true,
+    timeZone: 'local',
   };
 
   handleDateClick(arg : DateClickArg) {
-    console.log('A ver si va de una veeeeez ! ! ! dia clicado', arg.dateStr);
+    console.log('A ver si va de una veeeeez ! ! ! dia clicado?!', arg.dateStr);
 
     const eventsOfDay = this.calendarEvents.filter(event => event.date === arg.dateStr);
     this.selectedDayEvents.set(eventsOfDay);
